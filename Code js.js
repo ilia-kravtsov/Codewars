968613007be2c3c257ae0dfe7d4a7fbcd2ccce95
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-09-18 10:13:23 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -164,3 +164,16 @@ function howManydays(month){
   }
   return days;
 }
+
+// BP
+
+function howManydayss(monthh){
+  switch (monthh){
+     case 2: return 28
+     case 4:
+     case 6:
+     case 9:
+     case 11: return 30
+  }
+  return 31
+}
