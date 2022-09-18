f2323035efbb42a8df029bfeb649456ed84927c2
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-09-18 09:25:05 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -122,3 +122,12 @@ function popElement(arr){
 
 // BP
 
+const getLength = array => array.length;
+
+const getFirst = array => array[0];
+
+const getLast = array => array[array.length - 1];
+
+const pushElement = array => array.concat(0);
+
+const popElement = array => array.splice(0, array.length - 1);
