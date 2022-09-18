f268e7baf306f1d35c4c53d3a127f521fc39db08
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-09-18 11:28:40 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -245,3 +245,14 @@ function grabDoll(dolls){
   
   return bag;
 }
+
+// Training JS #12: loop statement --for..in and for..of
+
+function giveMeFive(obj){
+  let five=[];
+  for (let key in obj) {
+    if (key.length==5) five.push(key);
+    if (obj[key].length==5)five.push(obj[key]);
+  } 
+  return five;
+}
