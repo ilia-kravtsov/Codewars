17c7c7bc9cb7f9a268756c184bdf34e9382be310
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-09-18 10:53:58 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -205,3 +205,26 @@ function padIt(str,n){
   }
   return str;
 }
+
+// Training JS #10: loop statement --for
+
+function pickIt(arr){
+  var odd=[],even=[];
+  for(unit of arr)
+     (unit % 2 ? odd : even).push(unit)
+  return [odd,even];
+}
+
+//BP
+
+function pickIt (arr) {
+  
+  let odd = []
+  let even =[]
+      
+  for (var x of arr) {
+      ((x % 2) ? odd : even).push(x)    
+  }
+      
+  return [odd, even]
+}
