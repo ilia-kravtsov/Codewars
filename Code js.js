b31ff8f105758ec61ac79d23993d3dc644c4e9ea
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-09-18 10:32:34 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -177,3 +177,31 @@ function howManydayss(monthh){
   }
   return 31
 }
+
+// Training JS #9: loop statement --while and do..while
+
+function padIt(str,n){
+  while (n>0){
+  if (n%2){
+    str = "*" + str
+  } else {
+    str = str + "*"
+  }
+    n--
+  }
+  return str
+}
+
+//BP
+
+function padIt(str,n){
+  while(n>0){
+    if(n%2 === 0) {
+      str = str + "*";
+    }else{
+      str = "*" + str;
+    }
+    n --;
+  }
+  return str;
+}
