48a58da010e7e9c5d5b247e1d3e230521aa9b11d
ilia-kravtsov <kravtsov.i.s@mail.ru>
2022-09-20 13:09:28 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -296,3 +296,15 @@ function contamination(text, char){
   function repeatStr (n, s) {
     return s.repeat(n);
   }
+
+  function well(x) {
+    switch (x.filter(i => i === 'good').length) {
+      case 0:
+        return 'Fail!'
+      case 1:
+      case 2:
+        return 'Publish!'
+      default:
+        return 'I smell a series!'
+    }
+  }
