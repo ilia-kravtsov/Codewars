76fc0aaa13a5b4d8c3cb45bd6e528c84dfb8e7b4
@_ilia_kravtsov <kravtsov.i.s@mail.ru>
2022-10-24 16:55:36 +0300

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -321,3 +321,21 @@ function contamination(text, char){
   var str = 'Яблоки круглые и яблоки сочные.';
   var newstr = str.replace(re, 'апельсины');
   console.log(newstr); // апельсины круглые и апельсины сочные.
+
+  function points(games) {
+    // your code here
+    let sumOfPoints = 0
+    for(i = 0; i < games.length ; i++){
+      let gamesStrHolder = games[i]
+      let x = gamesStrHolder[0]
+      let y = gamesStrHolder[2]
+      if (x > y){
+        sumOfPoints += 3
+      }else if (x == y){
+        sumOfPoints += 1
+      }else if (x < y){
+        sumOfPoints += 0
+      }
+    }
+    return sumOfPoints
+  }
