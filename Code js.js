b35a48eadd896a4082f6e78b58ab7b799e108f07
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-01-26 13:43:49 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -731,3 +731,20 @@ function findAverage(array) {
   return CA
    
 }
+
+// _______________________ Duck Duck Goose solution
+
+duckDuckGoose = (players, goose) => players[(goose-1)%players.length].name
+
+// _______________________ Removing Elements
+
+/* 
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+const removeEveryOther = arr => arr.filter((item, index) => index % 2 ? false : item)
