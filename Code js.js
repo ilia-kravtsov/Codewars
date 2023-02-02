8ca324b72f96ed1ecd871e6ae7d5485ffd071ad2
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-02-02 12:46:54 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -841,3 +841,20 @@ const positiveSum = arr => {
 // or
 
 const positiveSumm = arr => arr.filter(x => x > 0).reduce((a, b) => a+b, 0);
+
+// __________________________________________________ Arguments to Binary addition
+
+/* 
+Given an array add all the number elements and return the binary equivalent of that sum; to make the code bullet proof anything else than a number should be addeded as 0 since it can't be addeded.
+
+If your language can handle float binaries assume the array won't contain float or doubles.
+
+arr2bin([1,2]) == '11'
+arr2bin([1,2,'a']) == '11'
+arr2bin([]) == '0'
+
+NOTE: NaN is a number too in javascript for decimal, binary and n-ary base
+This is a modification on the Kata: Array2Binary addition hope you like it
+*/
+
+const arr2bin = arr => arr.reduce((x,y) => x + (typeof y == 'number' ? y : 0), 0).toString(2)
