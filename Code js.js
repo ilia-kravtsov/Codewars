3acaceb7d8fbde2acae1fd105882042813ee01fc
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-02-09 17:01:41 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -947,12 +947,41 @@ function arrayMadness(a, b) {
 
 */
 const differenceInAges = ages => [Math.min.apply(null, ages), Math.max.apply(null, ages), Math.max.apply(null, ages) - Math.min.apply(null, ages)]
-// __________________________________________________ 
+// __________________________________________________ Count the Monkeys!
+
 
 /*
+You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
 
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+For example(Input --> Output):
+
+10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ 1 --> [1]
 */
+function monkeyCount(n) {
+  if (n === 1) {
+      return [n]
+  }
 
+  if(n > 1) {
+      let arr = []
+      for( let i = 1; i < n; i++) {
+           arr.push(i)
+      }
+      arr.push(n)
+      return arr
+  }
+}
+
+console.log(monkeyCount(1))
+
+// function monkeyCount(n) {
+//     for (var i = 0, arr = []; i < n; arr.push(++i));
+  
+//     return arr;
+//   }
 // __________________________________________________ 
 
 /*
