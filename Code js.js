393c672dd5bd0f522f0ee72b8011e905c97b82d7
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-02-12 13:37:42 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -1020,11 +1020,19 @@ The usage will be quite simple, like:
 Of course, the existing Array.prototype.filter() function has been undefined for the purposes of this Kata.
 */
 
-// __________________________________________________ 
+// __________________________________________________ Printing Array elements with Comma delimiters
 
-/*
+/* 
+Input: Array of elements
 
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
 */
+
+const printArray = a => a.join(',')
 
 // __________________________________________________ 
 
