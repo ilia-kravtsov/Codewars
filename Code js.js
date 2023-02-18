e2d1bb4081b1a146211a9077d060910bf6be932f
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-02-18 12:42:27 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -1159,12 +1159,24 @@ function countBy(x, n) {
   }
   return z;
 }
-// __________________________________________________ 
+// __________________________________________________ Sentences with Functions
+
+
 
 /*
-
+Adam(has(a(dog()))); // must return "Adam has a dog."
+The(name(of(the(dog(is(also(Adam()))))))); // must return "The name of the dog is also Adam."
 */
-
+const Adam = has => has ? `Adam ${has}`: 'Adam.';
+const has = a => `has ${a}`
+const a = dog => `a ${dog}`
+const dog = (is) => is ? `dog ${is}`: 'dog.'
+const The = (name) => `The ${name}`
+const name = (of) => `name ${of}`
+const of = (the) =>  `of ${the}`
+const the = (dog) => `the ${dog}`
+const is = (also) => `is ${also}`
+const also = (Adam) => `also ${Adam}`
 // __________________________________________________ 
 
 /*
