2b2b83f606ae26311dc5ebd56512020fd94439ba
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-07 11:20:56 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -1526,18 +1526,40 @@ Your function would return the following array:
 ["Eyes", "Glasses", "Monocles", "Telescopes"]
 */
 const sortByLength = array => array.sort((a,b) => a.length - b.length)
-// __________________________________________________ 
+// __________________________________________________ Find the middle element
+
 
 /*
+As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
 
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
 */
-
-// __________________________________________________ 
+function gimme (triplet) {
+  return triplet.indexOf(triplet.concat().sort(function(a,b){return a - b})[1])
+}
+// __________________________________________________ Are You Playing Banjo?
 
 /*
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
 
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
 */
-
+const areYouPlayingBanjo = name => name[0] === 'r' || name[0] === 'R' ? `${name} plays banjo` : `${name} does not play banjo`
 // __________________________________________________ 
 
 /*
