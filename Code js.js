6ef9ef691ac192ba942d15410a8dbeb37b01fcc6
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-17 11:58:52 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -1953,12 +1953,18 @@ function isLockNessMonster(s) {
   if (s.toLowerCase().includes('three fifty')) return true;
   else return false
 }
-// __________________________________________________ 
+// __________________________________________________Get number from string
 
 /*
+Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
 
+Function:
+
+getNumberFromString(s)
 */
-
+function getNumberFromString(s) {
+  return +s.replace(/[^0-9]/g, '');
+}
 // __________________________________________________ 
 
 /*
