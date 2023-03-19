7a2d5a45c80fb0fdb14eb1ee07b86ac2109d1576
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-19 12:17:34 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -1994,12 +1994,85 @@ You must output a string containing the two strings with the word ```' are '```
 var TempleStrings = function(obj, feature) {
   return `${obj} are ${feature}`
 }
-// __________________________________________________ 
+// __________________________________________________ Polish alphabet
 
 /*
+There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
 
+Your task is to change the letters with diacritics:
+
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+and print out the string without the use of the Polish letters.
+
+For example:
+
+"Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
 */
+function correctPolishLetters(s) {
+  return s.replace(/[ąćęłńóśźż]/g, c => "acelnoszz"["ąćęłńóśźż".indexOf(c)])
+}
 
+function correctPolishLetters (string) {
+  var stringArr = string.split("")
+  for (var i = 0; i<stringArr.length; i++){
+    switch (stringArr[i]){
+        case "ą":
+        stringArr[i] = "a";
+        break;
+        case "ć":
+        stringArr[i] = "c";
+        break;
+        case "ę":
+        stringArr[i] = "e";
+        break;
+        case "ł":
+        stringArr[i] = "l";
+        break;
+        case "ń":
+        stringArr[i] = "n";
+        break;
+        case "ó":
+        stringArr[i] = "o";
+        break;
+        case "ś":
+        stringArr[i] = "s";
+        break;
+        case "ź":
+        stringArr[i] = "z";
+        break;
+        case "ż":
+        stringArr[i] = "z";
+        break;
+        default:
+        stringArr[i];
+    }
+  }
+  return stringArr.join("");
+}
+
+var mapping = {
+  'ą': 'a',
+  'ć': 'c',
+  'ę': 'e',
+  'ł': 'l',
+  'ń': 'n',
+  'ó': 'o',
+  'ś': 's',
+  'ź': 'z',
+  'ż': 'z',
+};
+
+function correctPolishLetters(text) {
+  return text.split('').map(c => mapping[c] || c).join('');
+}
 // __________________________________________________ 
 
 /*
