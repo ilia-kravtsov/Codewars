d0b04b4f33d095b9bc088cf1aade907d36cb4ea4
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-22 13:21:15 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2260,18 +2260,52 @@ There is no preloaded code to help you. This is not about coding skills; think b
 function SubtractSum(n){
   return "apple"
 }
-// __________________________________________________ 
+// __________________________________________________ Count words
+
 
 /*
+Can you implement a function that will return number of words in a string?
 
+You have to ensure that spaces in string is a whitespace for real.
+
+Let's take a look on some examples:
+
+countWords("Hello"); // returns 1 as int
+countWords("Hello, World!") // returns 2
+countWords("No results for search term `s`") // returns 6
+countWords(" Hello") // returns 1
+// ... and so on
+What kind of tests we made for your code:
+
+Function have to count words and not spaces. You have to be sure that you doing it right
+Empty string has no words.
+String with spaces around should be trimmed.
+Non-whitespace (ex. breakspace, unicode chars) should be treated as a delimiter
+Doublecheck that words with chars like -, ', ` are counted right.
 */
+const countWords = str => str.split(/\s/).filter(x => x).length
+// __________________________________________________ Simple Change Machine
 
-// __________________________________________________ 
 
 /*
+Your task is to create a change machine.
 
+The machine accepts a range of specified coins and notes, it returns change in 20p and 10p coins in the minimum amount of pieces. A 50p for example would return two 20p pieces and one 10p piece. The machine will always try and return change, if you input a 20p for example it will return "10p 10p".
+
+The machine accepts these coins and notes: £5, £2, £1, 50p, 20p. Any coins and notes which are not accepted by the machine will be returned. If you were to put a £20 note into the machine for example, it would be returned to you and not broken into change.
+
+This change machine is programmed to accept and distribute strings rather than numbers. The change will be returned as one string with the change separated by single spaces & no commas. The values of the string will be descending.
 */
-
+function changeMe(moneyIn){
+  switch(moneyIn) {
+      case '£5': return '20p '.repeat(25).trim()
+      case '£2': return '20p '.repeat(10).trim()
+      case '£1': return '20p '.repeat(5).trim()
+      case '50p': return '20p 20p 10p'
+      case '20p': return '10p 10p'
+      default: return moneyIn
+  }
+}
 // __________________________________________________ 
 
 /*
