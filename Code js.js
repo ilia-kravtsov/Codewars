9ec87ec8896c2963ed66fa33133ad65461718ea7
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-24 12:11:25 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2336,18 +2336,40 @@ You can assume the array will always be valid, and there will always be one corr
 */
 const isSortedAndHow = array => JSON.stringify(array) === JSON.stringify([...array].sort((a, b) => a - b)) ? "yes, ascending" : JSON.stringify(array) === JSON.stringify([...array].sort((a, b) => b - a)) ? "yes, descending" : "no";
 
-// __________________________________________________ 
+// __________________________________________________ Two Oldest Ages
 
 /*
+The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age,  oldest age].
 
+The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+For example (Input --> Output):
+
+[1, 2, 10, 8] --> [8, 10]
+[1, 5, 87, 45, 8, 8] --> [45, 87]
+[1, 3, 10, 0]) --> [3, 10]
 */
+function twoOldestAges(ages){
+  let firstMax = Math.max.apply(null, ages)
+  let newArray = ages.filter((num, i) => i !== ages.indexOf(firstMax))
+  let secondMax = Math.max.apply(null, newArray)
+  return [secondMax, firstMax]
+}
 
-// __________________________________________________ 
+function twoOldestAges(ages){
+  return ages.sort(function(a,b){return a-b;}).slice(-2);
+}
+// __________________________________________________ Small enough? - Beginner
 
 /*
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+
 
 */
-
+const smallEnough = (a, limit) => a.map(num => num <= limit).indexOf(false) === -1
+smallEnough = (a, l) => a.every(e => e <= l)
 // __________________________________________________ 
 
 /*
