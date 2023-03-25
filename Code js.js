9cd7555ad43e33bff40a9cbdbddf4435565087e3
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-25 12:06:47 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2370,24 +2370,74 @@ You can assume all values in the array are numbers.
 */
 const smallEnough = (a, limit) => a.map(num => num <= limit).indexOf(false) === -1
 smallEnough = (a, l) => a.every(e => e <= l)
-// __________________________________________________ 
+// __________________________________________________ Find the capitals
+
 
 /*
+Instructions
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
 
+Example
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
 */
-
-// __________________________________________________ 
+var capitals = function (word) {
+    let result = []
+    for(let i = 0; i < word.length; i++){
+      if(word[i] === word[i].toUpperCase()){
+        result.push(word.indexOf(word[i]))
+      }
+    }
+    return result
+};
+// __________________________________________________ Check the exam
 
 /*
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
 
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
 */
 
-// __________________________________________________ 
+// __________________________________________________ Check the exam
+
 
 /*
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
 
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
 */
-
+function checkExam(array1, array2) {
+  let result = 0
+    for (let i = 0; i < array1.length; i++) {
+          if (array1[i] === array2[i]) {
+            result += 4
+          } else if (array1[i] !== array2[i] && array2[i] === '') {
+            result += 0
+          } else if (array1[i] !== array2[i]) {
+            result -= 1
+          }
+    }
+    if (result < 0) {
+      result = 0
+    }
+  return result  
+}
+checkExam = (x, y) => (x = y.reduce((s, e, i) => s + (e === x[i] ? 4 : e === '' ? 0 : -1), 0)) > 0 ? x : 0;
 // __________________________________________________ 
 
 /*
