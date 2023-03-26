b89bf364238eb6cabbefa974b55840547ad8582d
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-26 11:29:36 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2438,18 +2438,70 @@ function checkExam(array1, array2) {
   return result  
 }
 checkExam = (x, y) => (x = y.reduce((s, e, i) => s + (e === x[i] ? 4 : e === '' ? 0 : -1), 0)) > 0 ? x : 0;
-// __________________________________________________ 
+// __________________________________________________ Sum of Minimums!
+
 
 /*
+Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
 
+For Example:
+
+[ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+, [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+, [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+]
+So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+Note: You will always be given a non-empty list containing positive values.
+
+ENJOY CODING :)
 */
+function sumOfMinimums(arr) {
+  return arr.map(a => Math.min.apply(null, a)).reduce((a,b) => a + b)
+}
 
-// __________________________________________________ 
+const sumOfMinimums = arr => arr.reduce((a, v) => a + Math.min(...v), 0);
+// __________________________________________________ Row Weights
 
 /*
+Scenario
+Several people are standing in a row divided into two teams.
+The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
 
+Task
+Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+Notes
+Array size is at least 1.
+All numbers will be positive.
+Input >> Output Examples
+rowWeights([13, 27, 49])  ==>  return (62, 27)
+Explanation:
+The first element 62 is the total weight of team 1, and the second element 27 is the total weight of team 2.
+
+rowWeights([50, 60, 70, 80])  ==>  return (120, 140)
+Explanation:
+The first element 120 is the total weight of team 1, and the second element 140 is the total weight of team 2.
+
+rowWeights([80])  ==>  return (80, 0)
+Explanation:
+The first element 80 is the total weight of team 1, and the second element 0 is the total weight of team 2.
 */
+function rowWeights(array){
+  let odd = array.reduce((acc, value, i) => i % 2 === 0 ? acc += value : acc, 0)
+  let even = array.reduce((acc, value, i) => i % 2 !== 0 ? acc += value : acc, 0)
+  return [odd, even]
+}
 
+rowWeights=arr=>arr.reduce((a,b,i)=>(a[i%2]+=b,a),[0,0])
+
+function rowWeights(array){
+  var arr = [0, 0];
+  for (var i = 0; i < array.length; i++) {
+    i % 2 == 0 ? arr[0] += array[i] : arr[1] += array[i];
+  }
+  return arr;
+}
 // __________________________________________________ 
 
 /*
