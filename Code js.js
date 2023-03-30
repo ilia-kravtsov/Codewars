3b045ae5097af9ea2be254c411e301a39a1810c0
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-03-30 11:29:43 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2608,12 +2608,13 @@ The minimum sum obtained from summing each two integers product ,  9*0 + 8*2 +7*
 
 */
 const minSum = arr => arr.sort((a, b) => a - b).reduce((acc, val) => acc + val * arr.pop(), 0)
-// __________________________________________________ 
+// __________________________________________________ Divide and Conquer
+
 
 /*
-
+Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
 */
-
+const divCon = x =>  x.reduce((a, b) => b === +b ? a + b : a - +b, 0)
 // __________________________________________________ 
 
 /*
