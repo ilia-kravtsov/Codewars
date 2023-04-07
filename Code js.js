ce5f5409d3025b2c06f25a22e88cf5a0f233e259
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-04-07 11:10:36 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2768,11 +2768,20 @@ your function should return the following array:
 */
 const greetDevelopers = list => list.map(p => ({...p, greeting: `Hi ${p.firstName}, what do you like the most about ${p.language}?`}))
 const greetDevelopers = list => list.map(p => ({...p, greeting: `Hi ${p.firstName}, what do you like the most about ${p.language}?`}))
-// __________________________________________________ 
+// __________________________________________________max diff - easy
 
 /*
+You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
 
+lst contains integers, that means it may contain some negative numbers
+if lst is empty or contains a single element, return 0
+lst is not sorted
+[1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+[1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+Have fun!
 */
+const maxDiff = list => list.length === 0 || list.length === 1 ? 0 : Math.max(...list) - Math.min(...list);
+const maxDiff = list => list.length ? Math.max(...list) - Math.min(...list) : 0;
 
 // __________________________________________________ 
 
