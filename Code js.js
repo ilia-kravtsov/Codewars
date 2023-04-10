961106efae47f1fd13c23296c1722a80a6433d15
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-04-10 12:56:18 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2790,12 +2790,34 @@ const maxDiff = list => list.length ? Math.max(...list) - Math.min(...list) : 0;
 Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
 */
 const cubeOdd = arr => arr.some(e => e !== +e) ? undefined : arr.map(n => n ** 3).reduce((acc, n) => n % 2 !== 0 ? acc + n : acc, 0)
-// __________________________________________________ 
+// __________________________________________________ Lost number in number sequence
+
 
 /*
+An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
 
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+If no number was deleted from the starting array, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []).
 */
-
+function findDeletedNumber(arr, mixArr) {
+  if(arr.length === mixArr.length) return 0;
+  for (let n of arr) {
+    if (mixArr.indexOf(n) === -1)  return n;
+  }
+}
+function findDeletedNumber(arr, mixArr) {
+  let arrSum = arr.reduce((a,b) => a + b, 0)
+  let mixArrSum = mixArr.reduce((a,b) => a + b, 0)
+  return arrSum - mixArrSum
+}
+const findDeletedNumber = (arr, mixArr) =>
+  arr.find(val => !mixArr.includes(val)) || 0;
 // __________________________________________________ 
 
 /*
