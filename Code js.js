52d52ec3ae078cb98968047d8aa033fff61c261e
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-04-13 10:59:01 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2838,12 +2838,37 @@ arr=[2,169,13,-5,0,-1] n=4    ==> return 2
 arr=[2,1,3,3,1,2],     n=3    ==> return 2 
 */
 const nthSmallest = (arr, pos) => arr.sort((a,b) => a-b)[pos -1];
-// __________________________________________________ 
+// __________________________________________________ Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
 
 /*
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+Your task is to return one of the following strings:
+
+< firstName here >, < country here > of the first Python developer who has signed up; or
+There will be no Python developers if no Python developer has signed up.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+your function should return Victoria, Puerto Rico.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
 
 */
-
+function getFirstPython(list) {
+  let programmer = list.filter(p => p.language === 'Python')[0]
+  if (programmer) {
+      return `${programmer.firstName}, ${programmer.country}`
+  } else {
+      return 'There will be no Python developers'
+  }
+}
 // __________________________________________________ 
 
 /*
