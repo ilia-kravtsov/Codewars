14d0f91e9ec7ab3c8c569b9ff8b6d1370d9a4685
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-04-14 10:22:37 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -2869,12 +2869,33 @@ function getFirstPython(list) {
       return 'There will be no Python developers'
   }
 }
-// __________________________________________________ 
+// __________________________________________________ My Language Skills
 
 /*
+Task
+You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
 
+Note: the scores will always be unique (so no duplicate values)
+
+Examples
+{"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+{"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+{"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
 */
+function myLanguages(results) {
+  let result = []
+  for (const key in results) {
+    if (results[key] >= 60) {
+      result.push(key)
+      result.sort((a, b) => results[b] - results[a])
+    } 
+  }
+  return result
+}
 
+function myLanguages(results) {
+  return Object.keys(results).filter(r => results[r] > 59).sort((a,b) => results[b] - results[a]);
+}
 // __________________________________________________ 
 
 /*
