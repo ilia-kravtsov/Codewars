b1b2ed7ce39dcef581dd6ae8eb55c67a3b80532c
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-04-19 10:07:09 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -3056,12 +3056,15 @@ function menFromBoys(arr){
  result = (even.sort(function(a,b){return a-b})+","+odd.sort(function(a,b){return b-a}))
  return result.split(",").map(Number)
 }
-// __________________________________________________ 
+// __________________________________________________ Sort by Last Char
+
 
 /*
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
 
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
 */
-
+const last = x => x.split(" ").sort((a, b) => a.charCodeAt(a.length-1) - b.charCodeAt(b.length-1));
 // __________________________________________________ 
 
 /*
