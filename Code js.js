6ef6d2e372ea1275738481fd2e036878e8358703
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-06-04 10:04:37 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -3268,12 +3268,17 @@ spacify("hello world") // returns "h e l l o   w o r l d"
 */
 const spacify = str => [...str].join(' ');
 const spacify_mine = str => str.split('').join(' ');
-// __________________________________________________ 
+// __________________________________________________ Help the Fruit Guy
 
 /*
+Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
 
+Notes
+If the array is null/nil/None or empty you should return empty array ([]).
+The rotten fruit name will be in this camelcase (rottenFruit).
+The returned array should be in lowercase.
 */
-
+const removeRotten = bagOfFruits => !bagOfFruits ? [] : bagOfFruits.map(f => f.includes("rotten") ? f.replace("rotten", "").toLowerCase() : f)
 // __________________________________________________ 
 
 /*
