fa44f10dca1bb80a874cd9d9f62cd2c9df78f75c
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-06-24 09:25:48 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -3786,22 +3786,73 @@ ARRAYSFUNDAMENTALS
 Suggest kata description edits
 */
 
-// __________________________________________________ 
+// __________________________________________________The Vowel Code
+ 
 
 /*
-function dataReverse(data) {
-  let reversedData = [];
+Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
 
-  // Loop through input data in chunks of 8 bits and prepend them to the reversedData array
-  for (let i = 0; i < data.length; i += 8) {
-    reversedData = data.slice(i, i + 8).concat(reversedData);
-  }
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
 
-  return reversedData;
-}
+Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
 */
 
+function encode(string) {
+  const codeMap = {
+    'a': '1',
+    'e': '2',
+    'i': '3',
+    'o': '4',
+    'u': '5'
+  };
 
+  let encodedString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string.charAt(i);
+
+    if (codeMap.hasOwnProperty(char)) {
+      encodedString += codeMap[char];
+    } else {
+      encodedString += char;
+    }
+  }
+
+  return encodedString;
+}
+
+function decode(string) {
+  const codeMap = {
+    '1': 'a',
+    '2': 'e',
+    '3': 'i',
+    '4': 'o',
+    '5': 'u'
+  };
+
+  let decodedString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string.charAt(i);
+
+    if (codeMap.hasOwnProperty(char)) {
+      decodedString += codeMap[char];
+    } else {
+      decodedString += char;
+    }
+  }
+
+  return decodedString;
+}
 // __________________________________________________ 
 
 /*
