5d4b164ab344522ed948b40f16d759e8e8c31690
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-07-07 21:40:55 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -4169,12 +4169,26 @@ function isSolved(board) {
    // If no empty spots, it's a draw
    return 0;
  }
-// __________________________________________________ 
+// __________________________________________________ Vowel Count
+
 
 /*
+Return the number (count) of vowels in the given string.
 
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
 */
-
+function getCount(str) {
+  let vowelCount = 0;
+    const vowels = ['a','e','i','o','u'];
+    for(let i=0;i<str.length;i++){
+        if(vowels.includes(str[i])){
+            vowelCount++;
+        }
+    }
+    return vowelCount;
+}
 // __________________________________________________ 
 
 /*
