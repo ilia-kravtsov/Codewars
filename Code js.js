91fde7ec5d53afb3cbf984d3ae87c2cbd86131a8
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-07-29 09:55:51 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -4433,12 +4433,22 @@ The strings representing a given language will always be formatted in the same w
 The input array will always be valid and formatted as in the example above.
 */
 const isSameLanguage = list => list.every(item => item.language === list[0].language)
-// __________________________________________________
+// __________________________________________________ The Office IV - Find a Meeting Room
+
 
 /*
+Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
 
+In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+'X' --> busy
+'O' --> empty
+If all rooms are busy, return "None available!"
 */
-
+function meeting(x){
+  let result = x.findIndex(room => room === 'O')
+  return result === -1 ? "None available!" : result
+}
 // __________________________________________________
 
 /*
