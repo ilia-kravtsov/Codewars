61a9116b66ede1f51970da576f0a2390cb7d6c09
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-08-09 14:26:39 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -4497,12 +4497,41 @@ function crap(x, bags, cap){
     return 'Cr@p';
   }
 }
-// __________________________________________________
+// __________________________________________________Head, Tail, Init and Last
 
 /*
+Haskell has some useful functions for dealing with lists:
 
+$ ghci
+GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+λ head [1,2,3,4,5]
+1
+λ tail [1,2,3,4,5]
+[2,3,4,5]
+λ init [1,2,3,4,5]
+[1,2,3,4]
+λ last [1,2,3,4,5]
+5
+Your job is to implement these functions in your given language. Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
+
+| HEAD | <----------- TAIL ------------> |
+[  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+| <----------- INIT ------------> | LAST |
+
+head [x] = x
+tail [x] = []
+init [x] = []
+last [x] = x
+Here's how I expect the functions to be called in your language:
+
+head([1,2,3,4,5]); => 1
+tail([1,2,3,4,5]); => [2,3,4,5]
+Most tests consist of 100 randomly generated arrays, each with four tests, one for each operation. There are 400 tests overall. No empty arrays will be given. Haskell has QuickCheck tests
 */
-
+const head = input => input[0];
+const tail = input => input.slice(1);
+const init = input => input.slice(0, input.length-1)
+const last = input => input[input.length-1]
 // __________________________________________________
 
 /*
