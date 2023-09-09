5f07b2967bab1e32a93ee490a54a5f43bb99bc8b
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-09-09 23:06:28 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -5262,12 +5262,32 @@ function bingo(a) {
 
   return 'LOSE';
 }
-//_______________________________________________________________
+//_______________________________________________________________String array duplicate
 
 /*
+In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
 
+For example:
+
+dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+dup(["kelless","keenness"]) = ["keles","kenes"].
+
+Strings will be lowercase only, no spaces. See test cases for more examples.
+
+Good luck!
 */
-
+function dup(s) {
+  return s.map((str) => {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== str[i + 1]) {
+        result += str[i];
+      }
+    }
+    return result;
+  });
+};
 //_______________________________________________________________
 
 /*
