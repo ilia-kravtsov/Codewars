52bfc706d6f20efbfb7e0b2d6ae0b1df4b072e42
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-09-13 20:51:51 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -5353,18 +5353,79 @@ function beggars(values, n){
 
   return result;
 }
-//_______________________________________________________________
+//_______________________________________________________________Lottery Ticket
 
 /*
+Time to win the lottery!
 
+Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+Example ticket:
+
+[ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+
+Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+All inputs will be in the correct format. Strings on tickets are not always the same length.
 */
+function bingo(ticket, win) {
 
-//_______________________________________________________________
+  let miniWins = 0;
+  for (let i = 0; i < ticket.length; i++) {
+    const [str, num] = ticket[i]; // ABC 65
+    for (let j = 0; j < str.length; j++) {
+      if (str.charCodeAt(j) === num) {
+        miniWins++;
+        break;
+      }
+    }
+  }
+  
+  if (miniWins >= win) {
+    return 'Winner!';
+  } else {
+    return 'Loser!';
+  }
+}
+//_______________________________________________________________Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer
+
 
 /*
+You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
 
+Your task is to return a sequence which includes the developer who is the oldest. In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+  { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+your function should return the following array:
+
+[
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+]
 */
+function findSenior(list) {
+  let maxAge = 0;
+  
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].age > maxAge) {
+      maxAge = list[i].age;
+    }
+  }
 
+  return list.filter((developer) => developer.age === maxAge);
+}
+function findSenior(list) {
+	var maxAge = Math.max(...list.map(person => person.age));
+	return list.filter(person => person.age === maxAge);
+}
 //_______________________________________________________________
 
 /*
