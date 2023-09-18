cab241ea37272a102febcba7eac2b1325c5d81d6
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-09-18 13:23:05 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -5682,12 +5682,33 @@ function shiftedDiff(first,second){
 
   return -1;
 }
-//_______________________________________________________________
+//_______________________________________________________________Stanton measure
 
 /*
+The Stanton measure of an array is computed as follows: count the number of occurences for value 1 in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
 
+Write a function which takes an integer array and returns its Stanton measure.
+
+Examples
+The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+
+The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times in the array and 3 occurs 1 time.
 */
-
+const stantonMeasure = (array) => {
+  let preResult = 0
+  let result = 0
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] === 1) {
+          preResult++
+      }
+  }
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] === preResult) {
+          result++
+      }
+  }
+  return result
+} 
 //_______________________________________________________________
 
 /*
