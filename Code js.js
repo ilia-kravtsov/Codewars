901c340b43ea394b9f1f65eb8beafc4ddca1b7eb
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-09-27 22:30:37 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -6282,12 +6282,29 @@ zipWith( (a,b) => a+b,                  [0,1,2,3], [0,1,2,3] )  =>  [0,2,4,6]  /
     function zipWith(fn,a0,a1) {
         return Array.from({length: Math.min(a0.length, a1.length)}, (_, i) => fn(a0[i], a1[i]));
     }
-//_______________________________________________________________
+//_______________________________________________________________ Calculate Two People's Individual Ages
+
 
 /*
+Create a function that takes in the sum and age difference of two people, calculates their individual ages, and returns a pair of values (oldest age first) if those exist or null/None if:
 
+sum < 0
+difference < 0
+Either of the calculated ages come out to be negative
 */
-
+function getAges(sum,difference){
+  if (sum < 0 || difference < 0) {
+    return null
+  } else {
+    const second = (sum - difference) / 2
+    const first = sum - second
+    if (first < 0 || second < 0) {
+      return null
+    } else {
+      return [first, second]
+    }
+  }
+};
 //_______________________________________________________________
 
 /*
