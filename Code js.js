addbdaf2b8849d6a9d4eaea33eec8f7ebd6c0962
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-06 15:42:02 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -6725,12 +6725,25 @@ function extraPerfect(n){
   
   return result;
 }
-//_______________________________________________________________
+//_______________________________________________________________ Move 10
+
 
 /*
+Move every letter in the provided string forward 10 letters through the alphabet.
 
+If it goes past 'z', start again at 'a'.
+
+Input will be a string with length > 0.
 */
-
+function moveTen(s){
+  let result = "";
+  for (let i = 0; i < s.length; i++) {
+    let charCode = s.charCodeAt(i);
+    let shiftedCharCode = ((charCode - 97 + 10) % 26) + 97;
+    result += String.fromCharCode(shiftedCharCode);
+  }
+  return result
+}
 //_______________________________________________________________
 
 /*
