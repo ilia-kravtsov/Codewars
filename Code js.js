aa39979f35ef829c07f914bda5b29d0ca21196d1
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-13 09:50:07 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -6874,12 +6874,30 @@ function findDup( arr ){
   function findDup(arr){
     return arr.reduce((a,b,i)=>a^b^i,0);
   }
-//_______________________________________________________________
+//_______________________________________________________________ Numbers to Objects
+
 
 /*
+You will be given an array of numbers.
 
+For each number in the array you will need to create an object.
+
+The object key will be the number, as a string. The value will be the corresponding character code, as a string.
+
+Return an array of the resulting objects.
+
+All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
 */
-
+function numObj(s){
+  let result = []
+  for (let i = 0; i < s.length; i++) {
+      let step = {}
+      step[s[i]] = String.fromCharCode(s[i])
+      result.push(step)
+  }
+  return result
+}
+const numObj = ($) => $.map(el => ( {[el]: String.fromCodePoint(el)} ))
 //_______________________________________________________________
 
 /*
