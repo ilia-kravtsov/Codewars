c2cda5fd70a0cdee656382088a37548363246d73
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-15 16:27:40 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -6898,12 +6898,27 @@ function numObj(s){
   return result
 }
 const numObj = ($) => $.map(el => ( {[el]: String.fromCodePoint(el)} ))
-//_______________________________________________________________
+//_______________________________________________________________ The Office III - Broken Photocopier
+
 
 /*
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
 
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+Given a string of binary, return the version the photocopier gives you as a string.
 */
-
+function broken(x){
+  let start = [...x]
+  for (let i = 0; i < start.length; i++) {
+      if (+start[i]) {
+          start[i] = '0'
+      } else {
+          start[i] = '1'
+      }
+  }
+  return start.join('')
+}
 //_______________________________________________________________
 
 /*
