7e89582ec86ca219975c7cccb64b3661e845ff93
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-16 08:42:57 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -6934,12 +6934,26 @@ function broken(x){
   }
   return start.join('')
 }
-//_______________________________________________________________
+//_______________________________________________________________ Well of Ideas - Harder Version
+
 
 /*
+For every good kata idea there seem to be quite a few bad ones!
 
+In this kata you need to check the provided 2 dimensional array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+The sub arrays may not be the same length.
+
+The solution should be case insensitive (ie good, GOOD and gOOd all count as a good idea). All inputs may not be strings.
 */
-
+function well(x){
+  let newArr = x.flat().map(w => typeof w === 'string' ? w.toLowerCase() : w);
+  let count = 0
+  for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i] === 'good') count++;
+  }
+  return count === 0 ? 'Fail!' : count <= 2 ? 'Publish!' : 'I smell a series!'
+}
 //_______________________________________________________________
 
 /*
