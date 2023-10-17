ba4c13bec5a1b6aade933eee4d03f665e9baa7d4
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-17 17:47:49 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -6954,12 +6954,29 @@ function well(x){
   }
   return count === 0 ? 'Fail!' : count <= 2 ? 'Publish!' : 'I smell a series!'
 }
-//_______________________________________________________________
+//_______________________________________________________________ Is every value in the array an array?
 
 /*
+Is every value in the array an array?
 
+This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+
+Examples:
+
+[[1],[2]] => true
+['1','2'] => false
+[{1:1},{2:2}] => false
 */
-
+const arrCheck = value => {
+  let count = 0
+  for (let v of value) {
+    if (Array.isArray(v)) {
+      count++
+    }
+  }
+  return count === value.length
+}
+const arrCheck = a => a.every(Array.isArray) ;
 //_______________________________________________________________
 
 /*
