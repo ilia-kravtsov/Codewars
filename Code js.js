b391a8768d28611b870aa0e5aa20fd34e5be2a1f
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-18 06:22:59 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -6977,12 +6977,24 @@ const arrCheck = value => {
   return count === value.length
 }
 const arrCheck = a => a.every(Array.isArray) ;
-//_______________________________________________________________
+//_______________________________________________________________ Filter unused digits
+
 
 /*
+Given a varying number of integer arguments, return the digits that are not present in any of them.
 
+Example:
+
+[12, 34, 56, 78]  =>  "09"
+[2015, 8, 26]     =>  "3479"
+Note: the digits in the resulting string should be sorted.
 */
-
+function unusedDigits(...numbers) {
+  const allDigits = '0123456789';
+  const usedDigits = numbers.join('').split('').filter((digit, index, array) => array.indexOf(digit) === index);
+  const unusedDigits = allDigits.split('').filter(digit => !usedDigits.includes(digit));
+  return unusedDigits.sort().join('');
+}
 //_______________________________________________________________
 
 /*
