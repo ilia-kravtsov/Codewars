05f3fb97334cbd0dfcdeb4e4541ba322448f9b71
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-20 13:56:23 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -7024,12 +7024,35 @@ function scrollingText(text){
   }
   return rotations;
 }
-//_______________________________________________________________
+//_______________________________________________________________ Merge two arrays
+
 
 /*
+Write a function that combines two arrays by alternatingly taking elements from each array in turn.
 
+Examples:
+
+[a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+
+[1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+Points:
+
+The arrays may be of different lengths, with at least one character/digit.
+One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).
 */
-
+function mergeArrays(a, b) {
+  let result = []
+  let compareLength = a.length > b.length ? a.length : b.length
+  for (let i = 0; i < compareLength; i++) {
+      if (a[i]) {
+          result.push(a[i])
+      }
+      if (b[i]) {
+          result.push(b[i])
+      }
+  }
+  return result
+}
 //_______________________________________________________________
 
 /*
