49bc78ce885fb2a5d4608147734b80f3862b4596
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-23 19:26:58 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -7075,12 +7075,28 @@ instructions = [1,2,4,3,5]
 "5 4 3 2 1 liftoff!"
 */
 const liftoff = instructions => `${instructions.sort((a,b) => b-a).join(' ')} liftoff!`;
-//_______________________________________________________________
+//_______________________________________________________________ Multiply Word in String
+
 
 /*
+You are to write a function that takes a string as its first parameter. This string will be a string of words.
 
+You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
+
+Once you have the located string you are finally going to multiply by it the third provided parameter, which will also be an integer. You are additionally required to add a hyphen in between each word.
+
+Example
+
+modifyMultiply ("This is a string", 3, 5) 
 */
-
+function modifyMultiply (str,loc,num) {
+    let result = []
+    let necessaryWord = str.split(' ')[loc]
+    for (let i = 0; i < num; i++) {
+        result.push(necessaryWord)
+    }
+    return result.join('-')
+} 
 //_______________________________________________________________
 
 /*
