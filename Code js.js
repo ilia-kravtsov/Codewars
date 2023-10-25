320248adc350739e4dfedd30e6ecc54c06ef6461
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-25 13:27:28 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -7133,12 +7133,20 @@ function findOddNames(list) {
   }
   return result
 }
-//_______________________________________________________________
+//_______________________________________________________________ Is Integer Array?
+
 
 /*
+Write a function with the signature shown below:
 
+function isIntArray(arr) {
+  return true
+}
+returns true  / True if every element in an array is an integer or a float with no decimals.
+returns true  / True if array is empty.
+returns false / False for every other input.
 */
-
+const isIntArray = arr => arr ? arr.every(n => Number.isInteger(n)) : false;
 //_______________________________________________________________
 
 /*
