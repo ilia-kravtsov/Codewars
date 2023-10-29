f794a100f0063b435f00c3dface88171d798fbed
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-10-29 14:04:49 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -7305,12 +7305,16 @@ function fruit(reels, spins) {
 
   return 0;
 }
-//_______________________________________________________________
+//_______________________________________________________________ Sort Arrays (Ignoring Case)
+
 
 /*
+Sort the given array of strings in alphabetical order, case insensitive. For example:
 
+["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
 */
-
+const sortme = names => names.sort( (a,b) => a.toLowerCase().localeCompare(b.toLowerCase()) )
 //_______________________________________________________________
 
 /*
