39dfed12c8e9641fee0c4170c476913ed89d5853
ilia-kravtsov <kravtsov.i.s@mail.ru>
2023-10-30 07:05:12 +0300

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -1800,7 +1800,23 @@ function remove(s){
     return s[s.length - 1] == '!' ? s.slice(0, -1) : s;
 }
 const remove = s => s.replace(/!$/, '');
-// __________________________________________________ Reversing Words in a String
+
+// __________________________________________________ If you can't sleep, just count sheep!!
+
+/*
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+const countSheep = num => {
+    let result = ''
+    for (let i = 1; i <= num; i++) {
+        result += `${i} sheep...`
+    }
+    return result
+}
+//// __________________________________________________ Reversing Words in a String
 
 
 /*
