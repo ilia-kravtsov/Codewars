5d5088b26cdda27e777a7a6cab840e8ed401ea82
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-11-04 17:11:46 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -7522,12 +7522,70 @@ function countBits(num) {
   }
   return count;
 }
-//_______________________________________________________________
+//_______________________________________________________________ Split and then add both sides of an array together.
+
 
 /*
+You will receive an array as parameter that contains 1 or more integers and a number n.
 
+Here is a little visualization of the process:
+
+Step 1: Split the array in two:
+
+[1, 2, 5, 7, 2, 3, 5, 7, 8]
+      /            \
+[1, 2, 5, 7]    [2, 3, 5, 7, 8]
+Step 2: Put the arrays on top of each other:
+
+   [1, 2, 5, 7]
+[2, 3, 5, 7, 8]
+Step 3: Add them together:
+
+[2, 4, 7, 12, 15]
+Repeat the above steps n times or until there is only one number left, and then return the array.
+
+Example
+Input: arr=[4, 2, 5, 3, 2, 5, 7], n=2
+
+
+Round 1
+-------
+step 1: [4, 2, 5]  [3, 2, 5, 7]
+
+step 2:    [4, 2, 5]
+        [3, 2, 5, 7]
+
+step 3: [3, 6, 7, 12]
+
+
+Round 2
+-------
+step 1: [3, 6]  [7, 12]
+
+step 2:  [3,  6]
+         [7, 12]
+
+step 3: [10, 18]
+
+
+Result: [10, 18]
 */
+function splitAndAdd(arr, n) {
+  while (arr.length > 1 && n > 0) {
+    const half = Math.floor(arr.length / 2);
+    const firstHalf = arr.slice(0, half);
+    const secondHalf = arr.slice(half);
 
+    if (firstHalf.length < secondHalf.length) {
+      firstHalf.unshift(0);
+    }
+
+    arr = firstHalf.map((num, index) => num + (secondHalf[index] || 0));
+    n--;
+  }
+
+  return arr;
+}
 //_______________________________________________________________
 
 /*
