2b621727be8856908148ae26272d7f650f736e7a
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-11-24 14:29:54 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -8121,12 +8121,31 @@ function countInversions(array) {
   mergeSort(array);
   return inversions;
 }
-//_______________________________________________________________
+//_______________________________________________________________ Arrays Similar
+
 
 /*
+Write a function that determines whether the passed in sequences are similar. Similar means they contain the same elements, and the same number of occurrences of elements.
 
+var arr1 = [1, 2, 2, 3, 4],
+    arr2 = [2, 1, 2, 4, 3],
+    arr3 = [1, 2, 3, 4],
+    arr4 = [1, 2, 3, "4"]
+arraysSimilar(arr1, arr2); // Should equal true
+arraysSimilar(arr2, arr3); // Should equal false
+arraysSimilar(arr3, arr4); // Should equal false
 */
-
+function arraysSimilar(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    arr1.sort((a,b) => a - b)
+    arr2.sort((a,b) => a - b)
+    const newArr1 = JSON.stringify(arr1)
+    const newArr2 = JSON.stringify(arr2)
+    return newArr1 === newArr2
+  } else {
+    return false
+  }
+}
 //_______________________________________________________________
 
 /*
