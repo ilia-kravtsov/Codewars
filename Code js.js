bdb7fa7318a789b1d9202b3a7fd30e700abe5f03
ilia-kravtsov <kravtsov.i.s@mail.ru>
2023-12-01 20:49:03 +0300

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -8416,12 +8416,45 @@ function insideOut(x){
   }
   return arr.join(' ')
 }
-//_______________________________________________________________
+//_______________________________________________________________ Find the odd int
 
 /*
+Given an array of integers, find the one that appears an odd number of times.
 
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 */
+    function findOdd(A) {
+        let result = {}
+        for (let i = 0; i < A.length; i++) {
+            result[A[i]] = 0
+        }
 
+        const keys = Object.keys(result).map(n => +n)
+
+        for (let i = 0; i < A.length; i++) {
+            for (let j = 0; j < keys.length; j++) {
+                if (A[i] === keys[j]) {
+                    result[A[i]] = result[A[i]] + 1
+                }
+            }
+        }
+
+        let exit = 0
+        for (const key in result) {
+            if (result[key] % 2 !== 0) {
+                exit = key
+            }
+        }
+
+        return +exit
+    }
 //_______________________________________________________________
 
 /*
