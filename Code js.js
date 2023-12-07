fe71784ea4a55fa0d6d652842c0d79fdc6c7de5b
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-12-07 13:37:40 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -8638,12 +8638,41 @@ function flatten(...input) {
 var flatten=function(...arr){
   return arr.toString().split(",");
 }
-//_______________________________________________________________
+//_______________________________________________________________ Remember
+
 
 /*
+Write a function that takes a string and returns an array of the repeated characters (letters, numbers, whitespace) in the string.
 
+If a charater is repeated more than once, only show it once in the result array.
+
+Characters should be shown by the order of their first repetition. Note that this may be different from the order of first appearance of the character.
+
+Characters are case sensitive.
+
+For F# return a "char list"
+
+Examples:
+remember("apple") => returns ["p"]
+remember("apPle") => returns []          // no repeats, "p" != "P"
+remember("pippi") => returns ["p","i"]   // show "p" only once
+remember('Pippi') => returns ["p","i"]   // "p" is repeated first
 */
-
+function remember(str) {
+  var repeatedChars = [];
+  var seenChars = new Set();
+  
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
+    
+    if (str.indexOf(char) !== i && !seenChars.has(char)) {
+      repeatedChars.push(char);
+      seenChars.add(char);
+    }
+  }
+  
+  return repeatedChars;
+}
 //_______________________________________________________________
 
 /*
