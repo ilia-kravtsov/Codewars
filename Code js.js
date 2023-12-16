1503fec6d43a90de4d100843c84a938180eb334a
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-12-16 15:06:32 +0400

    Update Code js.js

--- Code js.js
+++ Code js.js
@@ -8855,11 +8855,39 @@ function interleave(...input) {
 
   return result;
 }
-//_______________________________________________________________
+//_______________________________________________________________ Partition On
 
 /*
+Write a function which partitions a list of items based on a given predicate.
 
+After the partition function is run, the list should be of the form [ F, F, F, T, T, T ] where the Fs (resp. Ts) are items for which the predicate function returned false (resp. true).
+
+NOTE: the partitioning should be stable; in other words: the ordering of the Fs (resp. Ts) should be preserved relative to each other.
+
+For convenience and utility, the partition function should return the boundary index. In other words: the index of the first T value in items.
+
+For example:
+
+var items = [1, 2, 3, 4, 5, 6];
+function isEven(n) {return n % 2 == 0}
+var i = partitionOn(isEven, items);
+// items should now be [1, 3, 5, 2, 4, 6]
+// i     should now be 3
 */
+function partitionOn(pred, items) {
+  let trueArr = []
+  let falseArr = []
+  for (let i = 0; i < items.length; i++) {
+      if (pred(items[i])) {
+          trueArr.push(items[i])
+      } else {
+          falseArr.push(items[i])
+      }
+  }
+  items.length = 0; 
+  items.push(...falseArr, ...trueArr);
+  return items.indexOf(trueArr[0])
+}
 //_______________________________________________________________
 
 /*
