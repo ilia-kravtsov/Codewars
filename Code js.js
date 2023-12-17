4d51f436f98cc27122824bde88947626435ac27d
ilia-kravtsov <kravtsov.i.s@mail.ru>
2023-12-17 10:46:26 +0300

    Human Readable Time is done

--- Code js.js
+++ Code js.js
@@ -8923,11 +8923,31 @@ function partitionOn(pred, items) {
   items.push(...falseArr, ...trueArr);
   return items.indexOf(trueArr[0])
 }
-//_______________________________________________________________
+//_______________________________________________________________ Human Readable Time
+
 
 /*
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
 
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
 */
+
+function humanReadable(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedHours = String(hours).padStart(2, '0');
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
 
 //_______________________________________________________________
 
