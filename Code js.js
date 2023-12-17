6d84505c4ceb29840f6fbcde38ddab59da5d27b3
ilia-kravtsov <kravtsov.i.s@mail.ru>
2023-12-17 09:35:38 +0300

    lottery ticket

--- Code js.js
+++ Code js.js
@@ -1,4 +1,4 @@
-seeДД
+NJseeДД
 // Training JS #1: create your first JS function and print "Hello World!"
 
 function helloWorld() {
@@ -6157,6 +6157,41 @@ function save(sizes, hd) {
       }
   }
   return result
+}
+//_______________________________________________________________Lottery Ticket
+
+/*
+Time to win the lottery!
+
+Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+
+Example ticket:
+
+[ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+
+Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+
+All inputs will be in the correct format. Strings on tickets are not always the same length.
+*/
+function bingo(ticket, win) {
+
+  let miniWins = 0;
+  for (let i = 0; i < ticket.length; i++) {
+    const [str, num] = ticket[i]; // ABC 65
+    for (let j = 0; j < str.length; j++) {
+      if (str.charCodeAt(j) === num) {
+        miniWins++;
+        break;
+      }
+    }
+  }
+  
+  if (miniWins >= win) {
+    return 'Winner!';
+  } else {
+    return 'Loser!';
+  }
 }
 // ___________________________________________Return the Missing Element
 
