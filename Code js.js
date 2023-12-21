154e6779e90c5b2701dfb40caa76cb18ca146ec8
ilia-kravtsov <kravtsov.i.s@mail.ru>
2023-12-21 13:17:45 +0300

    Update

--- Code js.js
+++ Code js.js
@@ -1216,6 +1216,46 @@ Example(Input => Output):
 0 => [0]
 */
 const digitize = n => (Array.from(String(n), Number)).reverse()
+
+// ________________________ Count of positives / sum of negatives
+
+/* 
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+  
+  if (input === [] || input === null) {
+      return []
+  }
+
+  let finalArray = []
+  let positiveArray = []
+  let negativeArray = []
+  let sumNegative = 0
+
+  positiveArray = input.filter(num => num > 0)
+  negativeArray = input.filter(num => num < 0)
+
+  if(positiveArray == 0 && negativeArray == 0) {
+    return []
+  }
+
+  for(let i = 0; i < negativeArray.length; i++) {
+      sumNegative += negativeArray[i];
+  }
+
+  return finalArray = [positiveArray.length, sumNegative]
+
+}
+
 // __________________________________________________ Count by X
 
 
