0583ac7f010ba36389cf07bf6388f03231e5303c
ilia-kravtsov <kravtsov.i.s@mail.ru>
2023-12-29 14:19:33 +0300

    5 kyu Rot13 is done

---
+++ 5_kyu/Rot13.js
@@ -0,0 +1,9 @@
+// Rot13
+
+/*
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+*/
+
+const rot13 = message => message.replace(/[a-zA-Z]/g, (c) => String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26));
