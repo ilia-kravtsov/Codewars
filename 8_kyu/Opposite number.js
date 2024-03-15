b716622998503eceba312d618ad0a63112d8cfad
ilia-kravtsov <kravtsov.i.s@mail.ru>
2024-03-15 09:10:00 +0300

    5 kyu Count IP Addresses is completed

---
+++ 8_kyu/Opposite number.js
@@ -0,0 +1,12 @@
+/*
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+const opposite = n => n * -1;
+const opposite_2 = n => -n;
