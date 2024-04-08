d00bd2dbe90676e276174e24dded4257e1444f8e
ilia-kravtsov <kravtsov.i.s@mail.ru>
2024-04-08 13:41:34 +0300

    5 kyu Rot 13 is completed

--- 5_kyu/Rot13.js
+++ 5_kyu/Rot13.js
@@ -7,3 +7,28 @@ Create a function that takes a string and returns the string ciphered with Rot13
 */
 
 const rot13 = message => message.replace(/[a-zA-Z]/g, (c) => String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26));
+
+/*
+
+Fork|Collect|
+How satisfied are you with this kata?VERYSOMEWHATNONE
+How can you tell an extrovert from an introvert at NSA?
+Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+
+I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it?
+According to Wikipedia, ROT13 is frequently used to obfuscate jokes on USENET.
+
+For this task you're only supposed to substitute characters. Not spaces, punctuation, numbers, etc.
+
+Test examples:
+
+"EBG13 rknzcyr." -> "ROT13 example."
+
+"This is my first ROT13 excercise!" -> "Guvf vf zl svefg EBG13 rkprepvfr!"
+*/
+
+function rot13(str) {
+  return str.replace(/[a-z]/gi, function(c) {
+      return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+  });
+}
