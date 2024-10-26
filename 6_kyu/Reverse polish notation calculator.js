ad8130188fdfdc6d6993968a49c0d2e953fd9fcf
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-10-26 11:29:06 +0300

    6_kyu\Reverse polish notation calculator.js is completed

---
+++ 6_kyu/Reverse polish notation calculator.js
@@ -0,0 +1,45 @@
+/*
+Your job is to create a calculator which evaluates expressions in Reverse Polish notation.
+
+For example expression 5 1 2 + 4 * + 3 - (which is equivalent to 5 + ((1 + 2) * 4) - 3 in normal notation) should evaluate to 14.
+
+For your convenience, the input is formatted such that a space is provided between every token.
+
+Empty expression should evaluate to 0.
+
+Valid operations are +, -, *, /.
+
+You may assume that there won't be exceptional situations (like stack underflow or division by zero).
+*/
+
+function calc(expr) {
+  if (!expr.trim()) return 0;
+
+  const stack = [];
+  const tokens = expr.split(" ");
+
+  tokens.forEach((token) => {
+    if (!isNaN(token)) {
+      stack.push(parseFloat(token));
+    } else {
+      const b = stack.pop();
+      const a = stack.pop();
+      switch (token) {
+        case "+":
+          stack.push(a + b);
+          break;
+        case "-":
+          stack.push(a - b);
+          break;
+        case "*":
+          stack.push(a * b);
+          break;
+        case "/":
+          stack.push(a / b);
+          break;
+      }
+    }
+  });
+
+  return stack.pop();
+}
