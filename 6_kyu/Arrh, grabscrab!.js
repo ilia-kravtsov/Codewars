977ed97051dd7070f4508fedc824e349ddd22450
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-10-31 09:30:10 +0300

    6_kyu\Arrh, grabscrab!.js is completed

---
+++ 6_kyu/Arrh, grabscrab!.js
@@ -0,0 +1,22 @@
+/*
+Pirates have notorious difficulty with enunciating. They tend to blur all the letters together and scream at people.
+
+At long last, we need a way to unscramble what these pirates are saying.
+
+Write a function that will accept a jumble of letters as well as a dictionary, and output a list of words that the pirate might have meant.
+
+For example:
+
+grabscrab( "ortsp", ["sport", "parrot", "ports", "matey"] )
+Should return ["sport", "ports"].
+
+Return matches in the same order as in the dictionary. Return an empty array if there are no matches.
+*/
+
+function grabscrab(anagram, dictionary) {
+  const sortWord = (word) => word.split("").sort().join("");
+
+  const sortedAnagram = sortWord(anagram);
+
+  return dictionary.filter((word) => sortWord(word) === sortedAnagram);
+}
