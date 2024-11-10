ef1104d7698f61f885a729c3231c98e2523c12d8
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-11-10 12:14:42 +0300

    6_kyu\New Cashier Does Not Know About Space or Shift.js is completed

---
+++ 6_kyu/New Cashier Does Not Know About Space or Shift.js
@@ -0,0 +1,57 @@
+/*
+Some new cashiers started to work at your restaurant.
+
+They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+
+All the orders they create look something like this:
+
+"milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+
+The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+
+Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+
+"Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+
+The kitchen staff expect the items to be in the same order as they appear in the menu.
+
+The menu items are fairly simple, there is no overlap in the names of the items:
+
+1. Burger
+2. Fries
+3. Chicken
+4. Pizza
+5. Sandwich
+6. Onionrings
+7. Milkshake
+8. Coke
+*/
+
+function getOrder(input) {
+  const menu = [
+    "burger",
+    "fries",
+    "chicken",
+    "pizza",
+    "sandwich",
+    "onionrings",
+    "milkshake",
+    "coke",
+  ];
+
+  let result = [];
+
+  for (let item of menu) {
+    const regex = new RegExp(item, "g");
+    const matches = input.match(regex);
+    if (matches) {
+      result.push(
+        ...matches.map(
+          (match) => match.charAt(0).toUpperCase() + match.slice(1)
+        )
+      );
+    }
+  }
+
+  return result.join(" ");
+}
