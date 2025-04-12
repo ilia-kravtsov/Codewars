fd219d3a253dff702ec1d86057f26427f79c61a6
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2025-04-12 12:08:26 +0300

    3_kyu\Sudoku Solver.js has completed

---
+++ 3_kyu/Sudoku Solver.js
@@ -0,0 +1,70 @@
+/*
+Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
+
+The Sudokus tested against your function will be "easy" (i.e. determinable; there will be no need to assume and test possibilities on unknowns) and can be solved with a brute-force approach.
+
+For Sudoku rules, see the Wikipedia article.
+
+var puzzle = [
+            [5,3,0,0,7,0,0,0,0],
+            [6,0,0,1,9,5,0,0,0],
+            [0,9,8,0,0,0,0,6,0],
+            [8,0,0,0,6,0,0,0,3],
+            [4,0,0,8,0,3,0,0,1],
+            [7,0,0,0,2,0,0,0,6],
+            [0,6,0,0,0,0,2,8,0],
+            [0,0,0,4,1,9,0,0,5],
+            [0,0,0,0,8,0,0,7,9]];
+
+sudoku(puzzle);
+/* Should return
+[[5,3,4,6,7,8,9,1,2],
+[6,7,2,1,9,5,3,4,8],
+[1,9,8,3,4,2,5,6,7],
+[8,5,9,7,6,1,4,2,3],
+[4,2,6,8,5,3,7,9,1],
+[7,1,3,9,2,4,8,5,6],
+[9,6,1,5,3,7,2,8,4],
+[2,8,7,4,1,9,6,3,5],
+[3,4,5,2,8,6,1,7,9]] 
+*/
+
+function sudoku(puzzle) {
+  function isValid(board, row, col, num) {
+    for (let x = 0; x < 9; x++) {
+      if (board[row][x] === num) return false;
+    }
+    for (let y = 0; y < 9; y++) {
+      if (board[y][col] === num) return false;
+    }
+    let startRow = Math.floor(row / 3) * 3;
+    let startCol = Math.floor(col / 3) * 3;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[startRow + i][startCol + j] === num) return false;
+      }
+    }
+    return true;
+  }
+
+  function solve() {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (puzzle[row][col] === 0) {
+          for (let num = 1; num <= 9; num++) {
+            if (isValid(puzzle, row, col, num)) {
+              puzzle[row][col] = num;
+              if (solve()) return true;
+              puzzle[row][col] = 0;
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  solve();
+  return puzzle;
+}
