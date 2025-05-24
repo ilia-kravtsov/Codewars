c5dd06fff75fd236514ad562602c101ecee19ecd
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2025-05-24 05:28:24 +0300

    6_kyu\Roman Numerals Decoder\Roman Numerals Decoder.js has completed

---
+++ 6_kyu/Roman Numerals Decoder/Roman Numerals Decoder.js
@@ -0,0 +1,27 @@
+function solution(roman) {
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    const current = romanMap[roman[i]];
+    const next = romanMap[roman[i + 1]];
+
+    if (next > current) {
+      total += next - current;
+      i++;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+}
