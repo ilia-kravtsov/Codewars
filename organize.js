be4addb214ad8e8f49615545792092e948a55763
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2025-05-26 13:08:06 +0300

    Rearrangement

---
+++ organize.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const folder = path.join(__dirname, "4-kyu");
+
+fs.readdirSync(folder).forEach((file) => {
+  const fullPath = path.join(folder, file);
+
+  if (fs.statSync(fullPath).isFile() && file.endsWith(".js")) {
+    const fileName = path.basename(file, ".js");
+    const dirPath = path.join(folder, fileName);
+    const indexPath = path.join(dirPath, "index.js");
+
+    const content = fs.readFileSync(fullPath, "utf8");
+
+    fs.mkdirSync(dirPath, { recursive: true });
+    fs.writeFileSync(indexPath, content);
+
+    fs.unlinkSync(fullPath); 
+  }
+});
