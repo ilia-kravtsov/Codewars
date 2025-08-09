8c0dc77213e813025001d870bbd5536ec5a4aff9
ilia-kravtsov <kravtsov.i.s@mail.ru>
2025-08-09 10:11:37 +0300

    Add solution - CodeHub

---
+++ 6-kyu/patterncraft-state/patterncraft-state.js
@@ -0,0 +1,31 @@
+class SiegeState {
+  constructor() {
+    this.canMove = false;
+    this.damage = 20;
+  }
+}
+​
+class TankState {
+  constructor() {
+    this.canMove = true;
+    this.damage = 5;
+  }
+}
+​
+class Tank {
+  constructor() {
+    this.state = new TankState();
+  }
+​
+  get canMove() {
+    return this.state.canMove;
+  }
+​
+  get damage() {
+    return this.state.damage;
+  }
+​
+  setState(state) {
+    this.state = state;
+  }
+}
