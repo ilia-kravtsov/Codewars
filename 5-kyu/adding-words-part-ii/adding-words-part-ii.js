b1da29c229b699dc4f97c5faa6ce62c227fcfd8b
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-03-13 20:54:17 +0300

    Add solution - CodeHub

---
+++ 5-kyu/adding-words-part-ii/adding-words-part-ii.js
@@ -0,0 +1,29 @@
+class Arith {
+  constructor(word) {
+    this.value = this.wordToNumber(word);
+  }
+​
+  add(word) {
+    return this.numberToWord(this.value + this.wordToNumber(word));
+  }
+​
+  wordToNumber(word) {
+    const small = {
+      zero: 0,
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+      ten: 10,
+      eleven: 11,
+      twelve: 12,
+      thirteen: 13,
+      fourteen: 14,
+      fifteen: 15,
+      sixteen: 16,
+      seventeen: 17,
