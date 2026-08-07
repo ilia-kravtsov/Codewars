417fe2f801ef52cfe2f43e090a0d0aeeabcc855f
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-08-07 05:58:52 +0300

    solve(6-kyu): Reusable memoisation

---
+++ 6-kyu/Reusable memoisation/index.js
@@ -0,0 +1,14 @@
+function memo(fn) {
+  const cache = new Map();
+​
+  return function(arg) {
+    if (cache.has(arg)) {
+      return cache.get(arg);
+    }
+​
+    const result = fn(arg);
+    cache.set(arg, result);
+​
+    return result;
+  };
+}
